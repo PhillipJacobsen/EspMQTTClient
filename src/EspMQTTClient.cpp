852ae4a5f59e11d14aafcef6b27aeb4c300edb72
PJ <33669966+PhillipJacobsen@users.noreply.github.com>
2020-04-29 01:31:42 -0600

    Update EspMQTTClient.cpp

--- src/EspMQTTClient.cpp
+++ src/EspMQTTClient.cpp
@@ -407,6 +407,16 @@ void EspMQTTClient::connectToMqttBroker()
   if (_enableSerialLogs)
     Serial.printf("MQTT: Connecting to broker @%s with client name \"@%s\" ... ", _mqttServerIp, _mqttClientName);
 
+//pj added April 26
+	uint8_t baseMac[6];
+	esp_read_mac(baseMac, ESP_MAC_WIFI_STA);		// Get MAC address for WiFi station
+	char baseMacChr[13] = {0};
+	sprintf(baseMacChr, "%02X%02X%02X%02X%02X%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);       // Example: B4E62DA8EF6D
+	const char*  _mqttClientName_PJ = (char*)baseMacChr;
+	_mqttClientName =  _mqttClientName_PJ;
+	
+//end pj added April 26
+
   bool success = _mqttClient.connect(_mqttClientName, _mqttUsername, _mqttPassword, _mqttLastWillTopic, 0, _mqttLastWillRetain, _mqttLastWillMessage, _mqttCleanSession);
 
   if (_enableSerialLogs)
